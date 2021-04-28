b45d505a7859ff49750cf06b06bd1d26c8de7ef9
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-04-28 22:14:26 +0530

    refactor project

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 export default function SearchMovies() {
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+
+        const query = "Jurassic Park";
+
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=5f39e211a11bf35db1cb521c36e5a6af&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Jurassic Park" />
